bf25b34bb57779caf6ccf885633102bb692646c4
SLOVAKIA <nasa.slovakia1@booster-precision.com>
2023-12-04 11:31:30 +0100

    +HostedService s DeleteOlderRecords

---
+++ MopromanWebApi/HostedService/SampleService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MopromanWebApi.Controllers;
+using MopromanWebApi.Models;
+
+namespace PeriodicBackgroundTaskSample;
+
+class SampleService
+{
+    private readonly ILogger<SampleService> _logger;
+    private readonly MopromanDbContext _context;
+    private const int MAX_POCET_ZAZNAMOV = 1000000;
+    private const int ZMAZANE_ZAZNAMY = 10;
+    private static int zmazane_zaznamy_celkom = 0;
+
+    //objekt implementujuci rozhranie ILogger sa dosadi automaticky
+    public SampleService(ILogger<SampleService> logger, MopromanDbContext context)
+    {
+        _logger = logger;        
+        _context = context;
+
+        
+    }
+
+    public async Task DoSomethingAsync()
+    {
+        //await Task.Delay(100);
+
+        int pocet_zaznamov = _context.Records.Count();
+
+        if (pocet_zaznamov > MAX_POCET_ZAZNAMOV)
+        {
+            List<Record> MazaneZaznamy = await _context.Records.Take(ZMAZANE_ZAZNAMY/*pocet_zaznamov - MAX_POCET_ZAZNAMOV*/).ToListAsync();
+            _context.Records.RemoveRange((IEnumerable<Record>)MazaneZaznamy);
+            //_ = _context.SaveChangesAsync();
+            await _context.SaveChangesAsync();
+            zmazane_zaznamy_celkom += 10;
+        } 
+
+        _logger.LogInformation(
+            "\n-----------------\n" +
+            "Pocet ZMAZANYCH ZAZNAMOV:"+ (ZMAZANE_ZAZNAMY/*pocet_zaznamov - MAX_POCET_ZAZNAMOV*/).ToString()+
+            "Pocet ZMAZANYCH ZAZNAMOV CELKOM:" + (zmazane_zaznamy_celkom).ToString() +
+            "\n-----------------");
+
+        //System.Windows.MessageBox.Show("Nepodarilo sa pripojiť k DB z nasledujúceho dôvodu!" + e.Message + "\n\nAplikácia bude skončená!", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+    }
+}
--- MopromanWebApi/Program.cs
+++ MopromanWebApi/Program.cs
@@ -2,13 +2,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using MopromanWebApi.Models;
+using PeriodicBackgroundTaskSample;
 
 string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddScoped<SampleService>();
+
+// Register as singleton first so it can be injected through Dependency Injection
+builder.Services.AddSingleton<PeriodicHostedService>();
+
+// Add as hosted service using the instance registered as singleton before
+builder.Services.AddHostedService(
+    provider => provider.GetRequiredService<PeriodicHostedService>());
+
+
+// Add services to the container.
 builder.Services.AddControllers();
 var connectionString = builder.Configuration.GetConnectionString("MopromanCS");
 builder.Services.AddDbContext<MopromanDbContext>(options =>
@@ -42,5 +53,18 @@ app.UseCors(MyAllowSpecificOrigins);
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/background", (
+    PeriodicHostedService service) =>
+{
+    return new PeriodicHostedServiceState(service.IsEnabled);
+});
+/*
+app.MapMethods("/background", new[] { "PATCH" }, (
+    PeriodicHostedServiceState state,
+    PeriodicHostedService service) =>
+{
+    service.IsEnabled = state.IsEnabled;
+});*/
 
 app.Run();
